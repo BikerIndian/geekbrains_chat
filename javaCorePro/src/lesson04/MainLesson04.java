a9ef55ebcf88faef5c5b3684d7bd13265a17f74d
Vladimir Svishch <5693031@gmail.com>
2020-07-26 17:57:27 +0300

    hw-3-04

---
+++ javaCorePro/src/lesson04/MainLesson04.java
@@ -0,0 +1,16 @@
+package lesson04;
+
+/*
+   ФИО:  		  Свищ Владимир Сергеевич
+   Факультет:     Geek University Android-разработки
+   Курс: 		  Java Core. Профессиональный уровень
+   Урок: 		  Урок 4. Многопоточность. Часть I
+   Дата правки:   26.07.2020
+*/
+
+public class MainLesson04 {
+
+    public static void main(String[] args) {
+
+    }
+}
